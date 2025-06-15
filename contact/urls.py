93ccc7a79a58accfd801d2c37f8c2a00f834bdbb
AhmedAboElFetooh <ahmedfetooh54@gmail.com>
2025-06-15 04:14:30 +0300

    editing static and media settings and connect template with static

---
+++ contact/urls.py
@@ -0,0 +1,8 @@
+
+# from . import views
+# from django.urls import path
+# app_name = 'contact'
+
+# urlspatterns = [
+#     path('contact/',views.contact_us)
+# ]
--- project/settings.py
+++ project/settings.py
@@ -119,10 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
-STATIC_FILES_DIRS = [
-    os.path.join(BASE_DIR,'project/static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
