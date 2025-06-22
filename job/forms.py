614af88707c9137ada674b35bc5ca2b4f0e9007f
AhmedAboElFetooh <ahmedfetooh54@gmail.com>
2025-06-22 12:42:17 +0300

    create form for add jobs in project

--- job/forms.py
+++ job/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Apply
+from .models import Apply ,JOB
 
 
 class ApplyForm(forms.ModelForm):
     class Meta :
         model = Apply
         fields = ['name','email','website','cv','cover_letter']
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = JOB
+        fields = '__all__'
+        exclude = 'slug',
--- job/views.py
+++ job/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from .models import JOB
 from django.core.paginator import Paginator
-from .forms import ApplyForm
+from .forms import ApplyForm ,JobForm
 
 # Create your views here.
 def job_list(request):
     job_list = JOB.objects.all()
-    paginator = Paginator(job_list,1)
+    paginator = Paginator(job_list,2)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -32,3 +33,18 @@ def job_detail(request,slug):
     }
     return render(request,'job/job_detail.html',context)
 
+def add_job(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST,request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.owner = request.user
+            myform.save()
+            return redirect(reverse('jobs:job_list'))
+    else :
+        form = JobForm
+
+    context ={
+        'form':form
+    }
+    return render(request,'job/add_job.html',context)
