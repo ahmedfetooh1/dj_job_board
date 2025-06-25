4467dcc02de76fa9ce236ec4567b94c4f5463a55
AhmedAboElFetooh <ahmedfetooh54@gmail.com>
2025-06-25 10:13:36 +0300

    add contact form and send messages in mail

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'contact',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -141,3 +142,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
+
+#email service
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pythondevelopper2005@gmail.com'
+EMAIL_HOST_PASSWORD = 'wbyc ranl qngw hbvb'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
